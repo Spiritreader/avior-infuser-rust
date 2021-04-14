9893933e08d99eb15162bdaf04b202921c226c2a
Sam <earthstamper@gmail.com>
2021-04-14 13:49:17 +0200

    delete code moved to library, add local package

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,12 @@
-mod cfg;
-mod db;
-use std::collections::BTreeMap;
-use std::{collections::HashMap, fmt};
+use avior_infuser_lib::*;
 use std::{env, error::Error};
 
 const CFG_PATH: &str = "config.json";
 
-pub struct InfuserError {
-    pub message: String,
-}
-
-impl fmt::Debug for InfuserError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl fmt::Display for InfuserError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl Error for InfuserError {}
-
 fn main() -> Result<(), Box<dyn Error>> {
     println!("avior infuser rust, version 0.1 - maneki-neko");
     let args: Vec<String> = env::args().collect();
-    println!("calling args: {:?}", &args);
+    println!("calling args: {:?}", &args[1..]);
     if args.len() < 4 {
         return Err("program needs exactly 3 arguments in this order: path, name, sub".into());
     }
@@ -37,7 +16,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let client_vec: Vec<db::Client> = db::get_clients(&mongo_client, &config.db_name)?;
 
     //client_vec.sort_by(|a, b| b.priority.cmp(&a.priority));
-    let grouped_clients = group_clients(&client_vec)?;
+    let grouped_clients = group_clients(&client_vec);
     let machine_jobcounts = db::get_machine_jobcount(&mongo_client, &config.db_name)?;
 
     let mut new_job = db::Job {
@@ -89,87 +68,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn get_eligible_client(
-    grouped_clients: BTreeMap<i32, Vec<&db::Client>>,
-    machine_jobcounts: HashMap<String, i32>,
-) -> Result<String, Box<dyn Error>> {
-    for (_, clients) in grouped_clients {
-        let mut job_count = i32::MAX;
-        let mut eligible: Option<db::Client> = None;
-        // loop over every client within a priority group
-        // rules: get the client...
-        // - with the lowest jobcount
-        // - that is online or has the ignore_online flag enabled
-        // - that hasn't reached its maximum job count
-        for client in clients {
-            let key = client
-                .id
-                .to_owned()
-                .ok_or(InfuserError {
-                    message: "a client in the database has no id, could not determine eligible clients".to_string(),
-                })?
-                .to_string();
-            if !client.online && !client.ignore_online {
-                continue;
-            }
-            if let Some(count) = machine_jobcounts.get(&key) {
-                if *count < job_count && *count < client.maximum_jobs {
-                    eligible = Some(client.to_owned());
-                    job_count = *count;
-                }
-            } else {
-                eligible = Some(client.to_owned());
-                job_count = 0;
-            }
-        }
-        // if a client was found within the priority group,
-        // return it, otherwise move on to the next one
-        match eligible {
-            Some(client) => {
-                println!("found eligible client {} with {} jobs", client.name, job_count);
-                return Ok(client.id.to_owned().unwrap().to_string());
-            }
-            None => (),
-        }
-    }
-    // if no client has been found, return an error
-    Err(Box::new(InfuserError {
-        message: "no eligible client found".to_string(),
-    }))
-}
 
-fn group_clients(client_vec: &Vec<db::Client>) -> Result<BTreeMap<i32, Vec<&db::Client>>, Box<dyn Error>> {
-    let mut dict = BTreeMap::new();
-    for client in client_vec {
-        let prio = client.priority;
-        dict.entry(prio).or_insert(Vec::new()).push(client);
-        /*
-        match dict.entry(prio) {
-            Entry::Vacant(e) => e.insert(vec![client]);
-            Entry::Occupied(mut e) => {}
-        }
-         */
-    }
-    Ok(dict)
-}
 
-#[allow(dead_code)]
-fn test_insert(
-    client: &mongodb::sync::Client,
-    insert_client: &db::Client,
-    config: &cfg::Config,
-) -> Result<(), Box<dyn Error>> {
-    let iid = db::insert_job(client, &config.db_name, insert_client, &mut db::Job {
-        id: None,
-        path: "\\\\vdr-u\\SDuRec\\Recording\\exists\\Geheimnisvolle Wildblumen_2021-04-10-14-58-01-arte HD (AC3,deu).ts".to_string(),
-        name: "Geheimnisvolle Wildblumen".to_string(),
-        subtitle: "Blütenpracht im Wald".to_string(),
-        assigned_client: db::AssignedClient::default(),
-        custom_parameters: Vec::new()
-    })?;
-    println!("{}", iid);
-    Ok(())
-}
 
 /*
 fn push_to_eligible(
