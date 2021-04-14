750f313894f0e832506f1e763708d6a42ddc83b9
Sam <earthstamper@gmail.com>
2021-04-14 18:23:09 +0200

    finish basic pushing logic
    Co-authored-by: FunnyPocketBook <FunnyPocketBook@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use avior_infuser_lib::*;
+use log::Log;
+use log::Logger;
 use std::{env, error::Error};
 
 const CFG_PATH: &str = "config.json";
+const IDENTITY: &str = "avior infuser rust, version 0.1 - maneki-neko";
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("avior infuser rust, version 0.1 - maneki-neko");
+    println!("{}", IDENTITY);
     let args: Vec<String> = env::args().collect();
     println!("calling args: {:?}", &args[1..]);
     if args.len() < 4 {
         return Err("program needs exactly 3 arguments in this order: path, name, sub".into());
     }
+
+    let mut logger: Logger = Log::new(IDENTITY);
+
     let config = cfg::read(CFG_PATH)?;
     let mongo_client = db::connect(&config)?;
     let _ = db::get_jobs(&mongo_client, &config.db_name)?;
@@ -35,41 +41,50 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     // try pushing job to eligible client
-    let result = get_eligible_client(grouped_clients, machine_jobcounts).and_then(|eligible_id| {
-        if let Some(found) = client_vec
-            .iter()
-            .find(|client| client.id.to_owned().unwrap_or_default().to_string() == eligible_id)
-        {
-            println!("pushing job to {}", &found.name);
-            return Ok(db::insert_job(&mongo_client, &config.db_name, found, &mut new_job)?);
-        }
-        Err(InfuserError {
-            message: "could not find eligible client candidate in client_vec".into(),
-        }
-        .into())
-    });
+    let mut result =
+        get_eligible_client(grouped_clients, machine_jobcounts).and_then(|(eligible_id, count, maximum)| {
+            if let Some(found) = client_vec
+                .iter()
+                .find(|client| client.id.to_owned().unwrap_or_default().to_string() == eligible_id)
+            {
+                let iid = db::insert_job(&mongo_client, &config.db_name, found, &mut new_job)?;
+                logger.add(&format!(
+                    "found eligible client {} with {}/{} job(s) and priority {}",
+                    found.name, count, maximum, found.priority
+                ));
+                return Ok(iid);
+            }
+            Err(InfuserError {
+                message: "could not find eligible client candidate in client_vec".into(),
+            }
+            .into())
+        });
 
     // try pushing job to default client
     if let Err(e) = result {
         println!("{:?}, pushing to default client {}", e, &config.default_client);
-        let default_client = client_vec.iter().find(|client| client.name == config.default_client);
-        match default_client {
-            Some(found) => {
-                db::insert_job(&mongo_client, &config.db_name, found, &mut new_job)?;
-            }
-            None => {
-                return Err(InfuserError {
-                    message: "could not find default candidate in client_vec".into(),
+        result = client_vec
+            .iter()
+            .find(|client| client.name == config.default_client)
+            .ok_or(
+                InfuserError {
+                    message: "could not find eligible default client in client_vec".into(),
                 }
-                .into())
-            }
+                .into(),
+            )
+            .and_then(|found| {
+                let iid = db::insert_job(&mongo_client, &config.db_name, found, &mut new_job)?;
+                Ok(iid)
+            });
+    }
+    match result {
+        Ok(_) => {}
+        Err(e) => {
+            println!("{:?}", e);
         }
     }
     Ok(())
 }
-
-
-
 
 /*
 fn push_to_eligible(
